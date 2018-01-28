f3a958b90db863782c7beb5c9f495a17fcd81eb8
vanemarnava <nechan10@gmail.com>
2018-01-27 21:03:49 -0300

    subiendo cambios, estilo google

--- app.js
+++ app.js
@@ -1,62 +1,41 @@
 
- function principal()
-{
-	do{
-		var answer = prompt("Ingresa el numero de su tarjeta de credito"); //pregunta el numero de la tarjeta
-		/*
-		if (answer === "" )
-		{
-			return alert("Debe ingresar algun numero valido");
-		}
-		else
-		{
-			isValidCard();
-		}
-		*/
-		isValidCard();
+do {
+  var answer = prompt('Ingresa el numero de su tarjeta de credito'); /* pregunta el numero de la tarjeta*/
+  if (answer !== '' && answer !== ' ') { /* si se ha ingresado algo o no hay espacio vacio*/
+    isValidCard(answer); /* pasar a la funcion de validar tarjeta*/
+  } else { /* si no es el caso*/
+    alert('Debes ingresar un numero'); /* pedir ingresar un numero valido*/
+  }	
+} while (answer === '' || answer === ' '); 
+function isValidCard(num) {	
+  var array = []; 
+  var size = num.length;
+  var lastPosition = size - 1;
+  var sum = 0;
 
-	}while (answer === " ");
-}
-  
+  for (var i = lastPosition; i >= 0; i--) {
+    array.push(parseInt(num[i]));
+  }
+  for (var j = 1; j <= array.length; j++) {
+    if ((j % 2) === 0) {
+      var number = array[j - 1] * 2;
+      if (number >= 10) {
+        number = number.toString();
+        var numOne = parseInt(number[0]);
+        var numberTwo = parseInt(number[1]);
+        array[j - 1] = numOne + numberTwo;
+      } else {
+        array[j - 1] = number;
+      }
+    }
+  }
 
-function isValidCard(num)
-{
-	
-	var newNumbers = num.split("") //pasar la info que se ingreso por la consola que esta en string a array (numbers), se coloca comilla sin espacio para que separe a cada numero.
-	
-	var array = [newNumbers]; //colocar los numeros en un array
-
-	array.reverse(); // revertir los numero ingresados en el mismo array
-
-	for(var i = 1 ; i < array.length; i + 2)// determinar las posiciones pares
-	{
-		//var operation = 0;
-
-		if( array[i] * 2 >= 10 )// los numeros de los casilleros pares se multiplican por dos y
-		{					//si el resultado da igual o mayor a cero, sus digitos se deben sumar
-			var suma = 0;
-			var arrayAux = []; // nuevo array auxiliar para colocar los digitos que se deben sumar
-  
-  			for (var x = 0 ; x < arrayAux.length ; x++)//recorrer cada digito que conforma la suma 
-  			{
-    			suma = suma + arrayAux[x]; //sumar dichos digitos
-  			}
-				return suma;//entrega la suma y debe ser ingresado en el array principal
-
-			if(  )
-			{
-				sum2 % 10 === 0	
-			}
-		}
-		else
-		{
-			return array[i];
-		}
-		//var sum = i * 2;
-	}  
-}
-
-principal();
-
-
-
+  for (var nu = 0; nu < array.length; nu++) { 
+    sum = sum = array[nu];
+  }
+  if (sum % 10 === 0) {
+    return alert('Numero de tarjeta valido');
+  } else if (sum % 10 !== 0) {
+    alert('Numero invalido');
+  }
+}  
